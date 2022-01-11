ed9e951158172ebcf823ad1e63f8dd63dc3485b4
ValentinVasilev <falioVasilev@gmail.com>
2022-01-11 11:12:00 +0200

    Models, Routes, Cors policy
    Models, Routes, enable Cors policy

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,30 +1,40 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 require("dotenv/config");
 
 const api = process.env.API_URL;
 
-app.use(express.json());
+const productsRouter = require("./routers/products");
+
+app.use(cors());
+app.options("*", cors());
+
+// Those are middlewares
+app.use(express.json()); // Here we parse the body from the request.
 app.use(morgan("tiny"));
 
-app.get(`${api}/products`, (req, res) => {
-  const product = {
-    id: 1,
-    name: "Valio",
-    image: "some_URL",
-  };
-  res.send(product);
-});
+// Routers
+app.use(`${api}/products`, productsRouter);
 
-app.post(`${api}/products`, (req, res) => {
-  const newProduct = req.body;
-  console.log(newProduct);
-  res.send(newProduct);
-});
+// Make connection to db before starting the server.
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "eshop",
+  })
+  .then(() => {
+    console.log("Db connection is ready~");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
-  //   console.log(api);
+  console.log(api);
   console.log("server is runing http//localhost:3000");
 });
