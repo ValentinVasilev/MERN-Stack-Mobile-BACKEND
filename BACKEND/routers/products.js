d3cb63fa186c577e3d448fcc21cbc39336f72e46
ValentinVasilev <falioVasilev@gmail.com>
2022-01-11 16:49:29 +0200

    Get Featured Products

--- BACKEND/routers/products.js
+++ BACKEND/routers/products.js
@@ -136,4 +136,18 @@ router.get("/get/count", async (req, res) => {
   });
 });
 
+// We find only the Featured products
+router.get("/get/featured/:count", async (req, res) => {
+  const count = req.params.count ? req.params.count : 0;
+  const products = await Product.find({
+    isFeatured: true,
+  }).limit(+count);
+
+  if (!products) {
+    res.status(500).json({ success: false });
+  }
+
+  res.send(products);
+});
+
 module.exports = router;
