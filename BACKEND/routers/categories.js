2e2bc0f145d14b0431c81a52259c6c610b7c9f0a
ValentinVasilev <falioVasilev@gmail.com>
2022-01-11 15:06:32 +0200

    Updated Category

--- BACKEND/routers/categories.js
+++ BACKEND/routers/categories.js
@@ -22,6 +22,27 @@ router.get("/:id", async (req, res) => {
   res.status(200).send(category);
 });
 
+router.put("/:id", async (req, res) => {
+  //  findByIdAndUpdate has 2 parameters, the 1st one is: /* The Id of the product */ and the second one is /* A object that contains the changed data */
+  const category = await Category.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      icon: req.body.icon,
+      color: req.body.color,
+    },
+    {
+      new: true, // This means that after changing the data I wan to return the newly updated data. Otherwise will give me the data that was changed(old one)
+    }
+  );
+
+  if (!category) {
+    res.status(400).json({ message: "The Category cannot be created" });
+  }
+
+  res.send(category);
+});
+
 router.post("/", async (req, res) => {
   let category = new Category({
     name: req.body.name,
