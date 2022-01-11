ee1f5a2fe523830e432127cd244d2bc49df3ac5a
ValentinVasilev <falioVasilev@gmail.com>
2022-01-11 16:30:38 +0200

    Delete Product

--- BACKEND/routers/products.js
+++ BACKEND/routers/products.js
@@ -2,10 +2,12 @@ const express = require("express");
 const { Category } = require("../models/category");
 const router = express.Router();
 const { Product } = require("../models/product");
+const mongoose = require("mongoose");
 
 // Get List of All Products
 router.get("/", async (req, res) => {
-  const productList = await Product.find().select("name image -_id"); // With Select method we can tell the exact info we want to get. | -_id | means we dont want Id of the product to be returned. with '-' we exclude.
+  // const productList = await Product.find().select("name image -_id"); // With Select method we can tell the exact info we want to get. | -_id | means we dont want Id of the product to be returned. with '-' we exclude.
+  const productList = await Product.find();
   res.send(productList);
 });
 
@@ -63,6 +65,11 @@ router.post("/", async (req, res) => {
 });
 
 router.put("/:id", async (req, res) => {
+  //Here we Validate if the Id is correct object.
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    res.status(400).send("Invalid Product Id");
+  }
+
   const category = await Category.findById(req.body.category);
 
   if (!category) {
@@ -97,4 +104,23 @@ router.put("/:id", async (req, res) => {
   res.send(product);
 });
 
+router.delete("/:id", async (req, res) => {
+  Product.findByIdAndRemove(req.params.id)
+    .then((product) => {
+      if (product) {
+        return res.status(200).json({
+          success: true,
+          message: `The product is deleted!`,
+        });
+      } else {
+        return res.status(404).json({
+          success: false,
+          message: `Product not found!`,
+        });
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({ success: false, error: err });
+    });
+});
 module.exports = router;
