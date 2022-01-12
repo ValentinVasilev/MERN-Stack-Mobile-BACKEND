982abfc32bf78cc60f65bded7387a1c911a1fb74
ValentinVasilev <falioVasilev@gmail.com>
2022-01-12 14:12:11 +0200

    Excluding REST API Routes From Authentication

--- BACKEND/helpers/jwt.js
+++ BACKEND/helpers/jwt.js
@@ -2,9 +2,23 @@ const expressJWT = require("express-jwt");
 
 function authJWT() {
   const secret = process.env.SECRET;
+  const api = process.env.API_URL;
   return expressJWT({
     secret,
     algorithms: ["HS256"],
+  }).unless({
+    path: [
+      // Here we tell the API what methods this URL can take
+      {
+        url: /\/api\/v1\/products(.*)/, // This Regex is a MUST, because without it URL like: "/get/featured/:count" will not be accessible
+        methods: ["GET", "OPTIONS"],
+      },
+      { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+
+      // Here we exclude routes to be Authenticated
+      `${api}/users/login`,
+      `${api}/users/register`,
+    ],
   });
 }
 
