39fd26fc566d6ed6112c03d0014ef146c68b0179
ValentinVasilev <falioVasilev@gmail.com>
2022-01-12 14:39:46 +0200

    Get User Count REST API

--- BACKEND/routers/users.js
+++ BACKEND/routers/users.js
@@ -69,4 +69,34 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.get("/get/count", async (req, res) => {
+  const usersCount = await User.countDocuments();
+
+  if (!usersCount) {
+    res.status(500).json({ success: false });
+  }
+  res.send({
+    usersCount: usersCount,
+  });
+});
+
+router.delete("/:id", async (req, res) => {
+  User.findByIdAndRemove(req.params.id)
+    .then((user) => {
+      if (user) {
+        return res.status(200).json({
+          success: true,
+          message: `The user is deleted!`,
+        });
+      } else {
+        return res.status(404).json({
+          success: false,
+          message: `User not found!`,
+        });
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({ success: false, error: err });
+    });
+});
 module.exports = router;
